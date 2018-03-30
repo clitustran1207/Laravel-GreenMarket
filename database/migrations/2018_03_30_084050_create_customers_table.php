f497d9db5728f27dbd41f0fd9b3a7c0beb2e9a45
APIGGYTU <clitustran1207@gmail.com>
2018-03-30 16:00:54 +0700

    Create tables in database

--- database/migrations/2018_03_30_084050_create_customers_table.php
+++ database/migrations/2018_03_30_084050_create_customers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCustomersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email')->unique();
-            $table->string('password');
             $table->string('fullname');
             $table->string('gender');
-            $table->date('birthday');
+            $table->string('birthday');
+            $table->string('email');
+            $table->string('address');
             $table->string('phone');
-            $table->tinyInteger('level');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('customers');
     }
 }
